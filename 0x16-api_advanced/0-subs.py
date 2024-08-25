3dd7c60cc1e355ea4edfcc962c1a0971b192b391
isahjohna <isahjohna@gmail.com>
2024-08-25 12:22:29 +0200

    fix task 0 error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Script that queries the number of subscribers on a given Reddit subreddit.
 """
 
 import requests
@@ -17,3 +17,9 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+
+
+if __name__ == "__main__":
+    subreddit = input("Enter subreddit name: ")
+    subscribers = number_of_subscribers(subreddit)
+    print(subscribers)
